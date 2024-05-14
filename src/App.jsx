efe612f8711c76d80bafb7b770fe42f1f363d9e7
anil-gehlot <anilgehlot2000@gmail.com>
2024-05-14 18:23:49 +0530

    Routes are defined in this file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import Logo from './components/Logo'
-import SearchButton from './Routes/SearchButton'
-import { Route, Routes } from 'react-router-dom'
-import Userinfo from './Routes/Userinfo'
-
+import Logo from "./components/Logo";
+import SearchButton from "./Routes/SearchButton";
+import { Route, Routes } from "react-router-dom";
+import Userinfo from "./Routes/Userinfo";
 
 function App() {
   return (
     <>
-      <div className='min-h-screen bg-black'>
-          <div className='container text-gray-200 py-3 '>
-            <Logo />
-            <Routes>
-              <Route path='/' element={<SearchButton />}  ></Route>
-              <Route path='/:name' element={<Userinfo />}  ></Route>
-
-            </Routes>
-          </div>
+      <div className="min-h-screen bg-black">
+        <div className="container text-gray-200 py-3 ">
+          <Logo />
+          <Routes>
+            <Route path="/" element={<SearchButton />}></Route>
+            <Route path="/:name" element={<Userinfo />}></Route>
+          </Routes>
         </div>
-      
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
