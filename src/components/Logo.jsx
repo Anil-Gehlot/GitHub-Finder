1f38c9844bc73b2615777d9f6a1d1d0dbd34c460
anil-gehlot <anilgehlot2000@gmail.com>
2024-05-14 18:20:17 +0530

    A Logo which is shown on the top of the Homepage

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Logo() {
   return (
-    <div className='flex pb-2 justify-center items-center border-gray-500 border-b'>
-      <img src='https://cdn3.iconfinder.com/data/icons/inficons/512/github.png' 
-       className='w-24 rounded-full'>
-       </img>
+    <div className="flex pb-2 justify-center items-center border-gray-500 border-b">
+      <img
+        src="https://cdn3.iconfinder.com/data/icons/inficons/512/github.png"
+        className="w-24 rounded-full"
+      ></img>
 
-       <h1></h1>
-       
+      <h1></h1>
     </div>
-  )
+  );
 }
