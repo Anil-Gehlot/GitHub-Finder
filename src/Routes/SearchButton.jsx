216762f7800ed2bfff653b10bca56f477c268356
anil-gehlot <anilgehlot2000@gmail.com>
2024-05-14 18:22:16 +0530

    Home Page with search button

--- src/Routes/SearchButton.jsx
+++ src/Routes/SearchButton.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Loading from "../components/Loading";
 import AllUsers from "../components/AllUsers";
 import UserNotFound from "../components/UserNotFound";
-import gitImage from "../assets/gitImage.jpg"
+import gitImage from "../assets/gitImage.jpg";
 import Footer from "../components/Footer";
 
 const SearchButton = () => {
@@ -26,13 +26,13 @@ const SearchButton = () => {
         }&client_secret=${import.meta.env.VITE_REACT_APP_GITHUB_CLIENT_SECRET}`
       );
       const data = await res.json();
-      
+
       if (data.items.length === 0) {
         setNoUserFound(true);
       } else {
         setUserData(data?.items);
       }
-    }else{
+    } else {
       window.location.reload();
     }
     setUser("");
@@ -41,41 +41,44 @@ const SearchButton = () => {
 
   return (
     <>
-    <div>
-      <div className="flex justify-center items-center h-11 my-5">
-        <input
-          type="text"
-          placeholder="Search github username..."
-          onChange={handleChange}
-          className="h-full md:w-1/3 w-2/3 text-gray-800 px-2 font-semobold outline-none "
-        />
+      <div>
+        <div className="flex justify-center items-center h-11 my-5">
+          <input
+            type="text"
+            placeholder="Search github username..."
+            onChange={handleChange}
+            className="h-full md:w-1/3 w-2/3 text-gray-800 px-2 font-semobold outline-none "
+            onKeyDown={(event) => event.key === "Enter" && findUser()}
+          />
 
-        <button
-          onClick={findUser}
-          value={user}
-          className="bg-teal-500 font-semibold px-4 h-full  "
-        >
-          Search
-        </button>
-      </div>
+          <button
+            onClick={findUser}
+            value={user}
+            className="bg-teal-500 font-semibold px-4 h-full  "
+          >
+            Search
+          </button>
+        </div>
 
-      {(!userData && !loading && !noUserFound)  && (
-        <div className="flex justify-center">
-            <img src={gitImage} alt="Git Logo" className="my-5 max-w-full h-auto" />
+        {!userData && !loading && !noUserFound && (
+          <div className="flex justify-center">
+            <img
+              src={gitImage}
+              alt="Git Logo"
+              className="my-5 max-w-full h-auto"
+            />
           </div>
         )}
-      
-      {loading ? (
-        <Loading />
-      ) : noUserFound ? (
-        <UserNotFound /> 
-      ) : (
-        <AllUsers userData={userData} />
-      )}
 
-      
-    </div>
-
+        {loading ? (
+          <Loading />
+        ) : noUserFound ? (
+          <UserNotFound />
+        ) : (
+          <AllUsers userData={userData} />
+        )}
+      </div>
+      <Footer />
     </>
   );
 };
