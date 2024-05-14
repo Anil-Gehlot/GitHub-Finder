ed64d941e4efe0826114f6dc79b60f87d010898b
anil-gehlot <anilgehlot2000@gmail.com>
2024-05-14 18:21:29 +0530

    This component will render when searched user will not found

--- src/components/UserNotFound.jsx
+++ src/components/UserNotFound.jsx
@@ -4,11 +4,10 @@ import notFound from "../assets/notFound.svg";
 const UserNotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center ">
-      <img src={notFound}  className="w-2/5 h-1/2  " />
+      <img src={notFound} className="w-2/5 h-1/2  " />
       <h2 className="text-2xl font-semibold">Oops! User Not Found</h2>
     </div>
   );
 };
 
 export default UserNotFound;
-
