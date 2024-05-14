844112199e3ea1a28c65505961cde30eba9c872a
anil-gehlot <anilgehlot2000@gmail.com>
2024-05-14 18:19:28 +0530

    Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,16 @@ const Footer = () => {
   return (
     <footer className="bg-black text-white mt-11">
       <div className="container mx-auto text-center">
-        <p className="text-sm">
-           Developed By  ❤️  Anil Gehlot.
+        <p className="text-md">
+          Developed By ❤️{" "}
+          <a
+            href="https://anil-gehlot.github.io/"
+            className="underline "
+            target="_blank"
+          >
+            Anil Gehlot
+          </a>{" "}
+          .
         </p>
       </div>
     </footer>
