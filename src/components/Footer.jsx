f0bb54d09e93a0e69dc10ae9c72f1d481be46da8
anil-gehlot <anilgehlot2000@gmail.com>
2024-05-14 12:08:59 +0530

    Footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white mt-11">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">
+           Developed By  ❤️  Anil Gehlot.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
