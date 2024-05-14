fbdcdb302fa44acd9bc103894bb3f3e2611a97fe
anil-gehlot <anilgehlot2000@gmail.com>
2024-05-14 18:20:55 +0530

    it helps to switch between Repositories and Followers tab

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Tabs = ( {type, setType} ) => {
+const Tabs = ({ type, setType }) => {
   return (
     <>
       <button
-          className={`${type === "repos" && "text-teal-400"}`}
-          onClick={() => setType("repos")}
-        >
-          Repositories
-        </button>
-        
-        <button
-          className={`${type === "followers" && "text-teal-400"}`}
-          onClick={() => setType("followers")}
-        >
-          Followers
-        </button>
-    </>
-  )
-}
+        className={`${type === "repos" && "text-teal-400"}`}
+        onClick={() => setType("repos")}
+      >
+        Repositories
+      </button>
 
-export default Tabs
+      <button
+        className={`${type === "followers" && "text-teal-400"}`}
+        onClick={() => setType("followers")}
+      >
+        Followers
+      </button>
+    </>
+  );
+};
+
+export default Tabs;
